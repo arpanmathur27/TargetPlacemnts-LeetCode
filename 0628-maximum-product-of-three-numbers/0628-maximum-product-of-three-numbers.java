35685e6076c4b5b5e16333b663f33be5d698252a
arpanmathur27 <arpanmathur27@gmail.com>
2026-07-26 22:48:02 +0530

    Time: 3 ms (86.33%), Space: 47.2 MB (93.89%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,10 +1,37 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);
-        int a=nums[0]*nums[1]*nums[nums.length-1];
-        int b=nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3] ;
-        if(a>b)return a;
-        return b;
+        int nmax1=Integer.MAX_VALUE;
+        int nmax2=Integer.MAX_VALUE;
+
+        int pmax1=Integer.MIN_VALUE;
+        int pmax2=Integer.MIN_VALUE;
+        int pmax3=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]<nmax1)
+            {
+                nmax2=nmax1;
+                nmax1=nums[i];
+            }
+            else if(nums[i]<nmax2)nmax2=nums[i];
+            
+            if(nums[i]>pmax1)
+            {
+                pmax3=pmax2;
+                pmax2=pmax1;
+                pmax1=nums[i];
+            }
+            else if(nums[i]>pmax2)
+            {
+                pmax3=pmax2;
+                pmax2=nums[i];
+            }
+            else if(nums[i]>pmax3)
+            {
+                pmax3=nums[i];
+            }
+        }
+        return Math.max(Math.max(nmax1 * nmax2 * pmax1, nmax1 * pmax2 * pmax1), pmax1 * pmax2 * pmax3);
 
         
     }
