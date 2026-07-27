1216fb5da0772f3cba0fec586f03d94aab466004
arpanmathur27 <arpanmathur27@gmail.com>
2026-07-27 21:12:29 +0530

    Time: 15 ms (98.27%), Space: 45.1 MB (95.29%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,6 +1,6 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        Arrays.sort(nums);
+        quickSort(nums,0,nums.length-1);
         int closestSum=nums[0]+nums[1]+nums[2];
         for(int i=0;i<nums.length-2;i++)
         {
@@ -19,5 +19,30 @@ class Solution {
         }
         return closestSum;
         
+    }
+    private void quickSort(int[] nums,int start,int end)
+    { if(start>=end)
+            return;
+        int pivot = nums[end];
+        int i=start;
+        for(int j=i;j<end;j++)
+        {
+            if(nums[j]<=pivot)
+                {
+                    swap(nums,i,j);
+                    i++;
+                }
+        }
+        swap(nums,i, end);
+        quickSort(nums,start,i-1);
+        quickSort(nums,i+1,end);
+
+    }
+    private static void swap(int[] nums,int i , int j)
+    {
+        int c= nums[i];
+        nums[i]=nums[j];
+        nums[j]=c;
+        return;
     }
 }
