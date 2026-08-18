add98e32164e50ba939f3fee529e885665b0c76f
arpanmathur27 <arpanmathur27@gmail.com>
2026-08-19 00:56:26 +0530

    Time: 1 ms (99.3%), Space: 44.8 MB (71.93%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,14 +1,51 @@
 class Solution {
-    public int largestInteger(int[] A, int k) {
-        int[] f = new int[51];
-        for (int x : A)
-            f[x]++;
-
-        int res = -1, n = A.length;
-        for (int i = 0; i < n; i++) 
-            if (k == n || (f[A[i]] == 1 && (k == 1 || i == 0 || i == n - 1)))
-                res = Math.max(res, A[i]);
-
-        return res;
+    static {
+        for(int i = 0; i <= 500; i++) {
+            largestInteger(new int[0], 0);
+        }
     }
+    public static int largestInteger(int[] nums, int k) {
+        int n = nums.length;
+        if(n == 0) return n;
+        // Case 2, find maximum num
+        if(n == k) {
+            int max = nums[0];
+            for(int i = 1; i < n; i++) {
+                max = Math.max(max, nums[i]);
+            }
+            return max;
+        }
+        // Case 3, find maximum with freq 1
+        if(k == 1) {
+            // Better than a HashMap here lol
+            int[] freq = new int[51];
+            for(int c : nums) {
+                freq[c]++;
+            }
+            int max = -1;
+            for(int c : nums) {
+                if(freq[c] >= 2) continue;
+                max = Math.max(max, c);
+            }
+            return max;
+        }
+        // Case 1, either nums[0] or nums[n-1]
+        // Check if they appear once
+        if(nums[0] == nums[n-1]) return -1;
+        boolean option1 = true;
+        boolean option2 = true;
+        for(int i = 1; i < n-1; i++) {
+            if(nums[i] == nums[0]) option1 = false;
+            if(nums[i] == nums[n-1]) option2 = false;
+        }
+        if(option1 && !option2) return nums[0];
+        if(!option1 && option2) return nums[n-1];
+        if(!option1 && !option2) return -1;
+        return Math.max(nums[0], nums[n-1]);
+    }
+
+    // Intuition: x can only appear 1 time in subarrays of size k, if:
+    // - It's at index 0 or index n-1 AND only appears once
+    // - k == n
+    // - k == 1 -> Find maximum single freq
 }
